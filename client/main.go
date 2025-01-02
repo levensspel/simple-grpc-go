eb6cf188eb82914b601239cdb8e7cccca9e0472d
levensspel <levensspiel@gmail.com>
2025-01-02 17:24:32 +0800

    feat: init

---
+++ client/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"time"
+
+	pb "github.com/levensspel/simple-grpc-go/proto/user"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	addr = flag.String("addr", "localhost:50051", "the address to connect to")
+)
+
+func main() {
+	flag.Parse()
+
+	// Set up a connection to the server.
+	conn, err := grpc.NewClient(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewUserServiceClient(conn)
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.RegisterUser(ctx, &pb.RegisterUserRequest{
+		Name:     "Your Name",
+		Username: "your_username",
+		Email:    "example@email.com",
+		Password: "SuperSecretPassword",
+	})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Returned User ID: %d", r.GetUserId())
+}
---
+++ server/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"math/rand"
+	"net"
+
+	pb "github.com/levensspel/simple-grpc-go/proto/user"
+	"google.golang.org/grpc"
+)
+
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
+type server struct {
+	pb.UnimplementedUserServiceServer
+}
+
+func (s *server) RegisterUser(_ context.Context, in *pb.RegisterUserRequest) (*pb.RegisterUserResponse, error) {
+	log.Printf("Received Name: %v", in.GetName())
+	log.Printf("Received Username: %v", in.GetUsername())
+	log.Printf("Received Email: %v", in.GetEmail())
+
+	return &pb.RegisterUserResponse{UserId: rand.Uint32()}, nil
+}
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterUserServiceServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
