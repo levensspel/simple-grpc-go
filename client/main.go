9e1c57ccbee884ad5ac1e6c473229ce156c45235
levensspel <levensspiel@gmail.com>
2025-01-03 15:16:37 +0800

    feat: add 'role' property with type 'oneof' to RegisterUserRequest Client can set the 'role' value with one of the defined 'message' type as follows: UserRole, MembershipRole, CreatorRole.
    The 'oneof' allows us to have different properties for each role.
    For example, 'UserRole' can just have one 'account_level' property while
    'MembershipRole can have 'account_level' and 'perks'.
    
    It is not necessary for every role to have 'account_level' like 'CreatorRole'
    that only have 'permission' property.

--- client/main.go
+++ client/main.go
@@ -27,21 +27,80 @@ func main() {
 	c := pb.NewUserServiceClient(conn)
 
 	// Contact the server and print out its response.
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
-	r, err := c.RegisterUser(ctx, &pb.RegisterUserRequest{
-		Name:     "Your Name",
-		Username: "your_username",
-		Email:    "example@email.com",
-		Password: "SuperSecretPassword",
+
+	// Register user with role "User"
+	user, err := c.RegisterUser(ctx, &pb.RegisterUserRequest{
+		Name:     "User Name",
+		Username: "user_username",
+		Email:    "user@email.com",
+		Password: "$2y$10$MslQAgEjHya7lnOSpQgtfeUGaQyUDCIGAO3lmSuFRiH4XnyZBHHfi",
 		Settings: map[string]string{
 			"Theme":    "dark",
 			"Language": "id",
+		},
+		Role: &pb.RegisterUserRequest_UserRole{
+			UserRole: &pb.UserRole{
+				AccountLevel: 1,
+			},
 		},
 	})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
-	log.Printf("Returned User ID: %d", r.GetUserId())
-	log.Printf("Returned Email Status: %d", r.GetEmailStatus())
+	log.Printf("Returned User ID: %d", user.GetUserId())
+	log.Printf("Returned Email Status: %d", user.GetEmailStatus())
+	log.Printf("")
+
+	// Register user with role "Membership"
+	member, err := c.RegisterUser(ctx, &pb.RegisterUserRequest{
+		Name:     "Member Name",
+		Username: "member_username",
+		Email:    "member@email.com",
+		Password: "$2y$10$g.1vwDij8rB10cS4RTUOEOmF7Nart2G.hUImUmVuED2dEk1Gn1jHe",
+		Settings: map[string]string{
+			"Theme":    "light",
+			"Language": "en",
+		},
+		Role: &pb.RegisterUserRequest_MembershipRole{
+			MembershipRole: &pb.MembershipRole{
+				AccountLevel: 3,
+				Perks: []pb.MembershipPerks{
+					pb.MembershipPerks_PERKS_FREE_ADS,
+					pb.MembershipPerks_PERKS_BACKGROUND_PLAY,
+				},
+			},
+		},
+	})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Returned Member ID: %d", member.GetUserId())
+	log.Printf("Returned Member's Email Status: %d", member.GetEmailStatus())
+
+	// Register user with role "User"
+	creator, err := c.RegisterUser(ctx, &pb.RegisterUserRequest{
+		Name:     "Creator Name",
+		Username: "creator_username",
+		Email:    "creator@email.com",
+		Password: "$2y$10$g.1vwDij8rB10cS4RTUOEOmF7Nart2G.hUImUmVuED2dEk1Gn1jHe",
+		Settings: map[string]string{
+			"Theme":    "light",
+			"Language": "id",
+		},
+		Role: &pb.RegisterUserRequest_CreatorRole{
+			CreatorRole: &pb.CreatorRole{
+				Permissions: []pb.RolePermission{
+					pb.RolePermission_PERMISSION_UPLOAD_HIGHER_VIDEO_QUALITY,
+					pb.RolePermission_PERMISSION_MONETIZE_VIDEO,
+				},
+			},
+		},
+	})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Returned Member ID: %d", creator.GetUserId())
+	log.Printf("Returned Member's Email Status: %d", creator.GetEmailStatus())
 }
--- server/main.go
+++ server/main.go
@@ -24,9 +24,28 @@ func (s *server) RegisterUser(_ context.Context, in *pb.RegisterUserRequest) (*p
 	log.Printf("Received Name: %v", in.GetName())
 	log.Printf("Received Username: %v", in.GetUsername())
 	log.Printf("Received Email: %v", in.GetEmail())
+	log.Printf("Received Password (hashed): %v", in.Password)
+
+	if in.GetUserRole() != nil {
+		log.Printf("Received User Role: %v", in.GetUserRole())
+		log.Printf("Received User Account Level: %v", in.GetUserRole().GetAccountLevel())
+	}
+
+	if in.GetMembershipRole() != nil {
+		log.Printf("Received Membership Role: %v", in.GetMembershipRole())
+		log.Printf("Received Membership Account Level: %v", in.GetMembershipRole().GetAccountLevel())
+		log.Printf("Received Membership Perks List: %v", in.GetMembershipRole().GetPerks())
+	}
+
+	if in.GetCreatorRole() != nil {
+		log.Printf("Received Creator Role: %v", in.GetCreatorRole())
+		log.Printf("Received Role Permissions: %v", in.GetCreatorRole().GetPermissions())
+	}
+
 	for k, v := range in.GetSettings() {
 		log.Printf("Received Settings->%s: %s", k, v)
 	}
+	log.Printf("")
 
 	return &pb.RegisterUserResponse{
 		UserId:      rand.Uint32(),
@@ -40,7 +59,9 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+
 	s := grpc.NewServer()
+
 	pb.RegisterUserServiceServer(s, &server{})
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
